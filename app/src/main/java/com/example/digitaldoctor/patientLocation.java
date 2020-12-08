1b2a6bf4ee7403c1a2932c4cca16a4ac84858498
Tapan Panchal <tapan007panchal@gmail.com>
2020-12-08 19:33:12 +0530

    gmap api I

---
+++ app/src/main/java/com/example/digitaldoctor/patientLocation.java
@@ -0,0 +1,123 @@
+package com.example.digitaldoctor;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class patientLocation extends AppCompatActivity {
+
+    //Initialize Variables
+    Spinner spType;
+    Button btFind;
+    SupportMapFragment supportMapFragment;
+    GoogleMap map;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    double currentLat = 0, currentLong = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_location);
+
+        spType = findViewById(R.id.sp_type);
+        btFind = findViewById(R.id.bt_find);
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+        String[] hospitalTypeList = {"hospital", "atm"};
+        String[] hospitalNameList = {"Kribhco Hospital", "New Civil Hospital"};
+
+        //set adapter on spinner
+        spType.setAdapter(new ArrayAdapter<>(patientLocation.this
+                , android.R.layout.simple_spinner_dropdown_item, hospitalNameList));
+
+        //initialize fused...blah
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        //check permission
+        if (ActivityCompat.checkSelfPermission(patientLocation.this
+                , Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            //when permission granted call method
+            getCurrentLocation();
+        }
+        else{
+            ActivityCompat.requestPermissions(patientLocation.this
+            ,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+
+        btFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int i = spType.getSelectedItemPosition();
+                String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json" +
+                        "?location=" + currentLat + "," + currentLong + "&radius=5000" +
+                        "&type=" + hospitalNameList[i] +
+                        "&sensor=true" + "&key=" + getResources().getString(R.string.google_map_key);
+            }
+        });
+
+    }
+
+    private void getCurrentLocation() {
+        //initialize task location
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null){
+                    currentLat = location.getLatitude();
+                    currentLong = location.getLongitude();
+
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+                            map = googleMap;
+                            map.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(currentLat,currentLong), 10
+                            ));
+
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == 44){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getCurrentLocation();
+            }
+        }
+    }
+}
